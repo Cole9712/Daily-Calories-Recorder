364c71a3e58bdf1d35938e5fe0b2672c30ea86ef
Hao Wu <hwa129@sfu.ca>
2020-05-15 00:40:02 -0700

    Modify User Info Activity

--- app/src/main/java/project/cole/dailynutritionhelper/UserInfoActivity.java
+++ app/src/main/java/project/cole/dailynutritionhelper/UserInfoActivity.java
@@ -1,8 +1,11 @@
 package project.cole.dailynutritionhelper;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -12,7 +15,10 @@ import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.text.MessageFormat;
 
 public class UserInfoActivity extends AppCompatActivity {
     public static final String tableName = "user_info";
@@ -38,12 +44,53 @@ public class UserInfoActivity extends AppCompatActivity {
         userWeight = findViewById(R.id.weightInput);
         userHeight = findViewById(R.id.heightInput);
         saveButton = findViewById(R.id.saveButton);
-
+        SharedPreferences sharedPreferences = getSharedPreferences(tableName, MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
 
         userInputContainer = (ConstraintLayout) findViewById(R.id.loginContainer);
         animationDrawable = (AnimationDrawable) userInputContainer.getBackground();
         animationDrawable.setEnterFadeDuration(5000);
         animationDrawable.setExitFadeDuration(2000);
+
+        userName.setText(sharedPreferences.getString("userName", null));
+        if (sharedPreferences.getString("userName", null) != null) {
+            if (sharedPreferences.getInt("userGenderPos", 0) == 1) {
+                userGender.setSelection(1);
+            }
+            userAge.setText(String.valueOf(sharedPreferences.getInt("userAge", 19)));
+            userWeight.setText(sharedPreferences.getString("userWeightInKgs", null));
+            userHeight.setText(sharedPreferences.getString("userHeightInMeters", null));
+            userActivityLevel.setSelection(sharedPreferences.getInt("userActivityLevelPos", 0), false);
+        }
+
+        userGender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                editor.putInt("userGenderPos", position);
+                editor.apply();
+//                Log.d("GenderSelection", "onItemSelected: "+position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                editor.putInt("userGenderPos", 0);
+                editor.apply();
+            }
+        });
+
+        userActivityLevel.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                editor.putInt("userActivityLevelPos", position);
+                editor.apply();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                editor.putInt("userActivityLevelPos", 0);
+                editor.apply();
+            }
+        });
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +101,7 @@ public class UserInfoActivity extends AppCompatActivity {
                     Snackbar.make(v, "Missing Essential Information", Snackbar.LENGTH_SHORT).show();
                 } else {
                     saveUserInfo(v);
+
                 }
             }
 
@@ -62,41 +110,26 @@ public class UserInfoActivity extends AppCompatActivity {
 
     }
 
-    private void saveUserInfo(View v) {
+    private void saveUserInfo(final View v) {
         SharedPreferences sharedPreferences = getSharedPreferences(tableName, MODE_PRIVATE);
         final SharedPreferences.Editor editor = sharedPreferences.edit();
         double userBMR = 0;
         double caloriesNeed = 0;
-        double carbohydratesNeed = 0;
-        double proteinsNeed = 0;
-        double fatsNeed = 0;
+        double minCarbohydratesNeed = 0;
+        double maxCarbohydratesNeed = 0;
+        double minProteinsNeed = 0;
+        double maxProteinsNeed = 0;
+        double minFatsNeed = 0;
+        double maxFatsNeed = 0;
 
         editor.putString("userName", userName.getText().toString());
         editor.putInt("userAge", Integer.parseInt(userAge.getText().toString()));
-        editor.putFloat("userWeight", (float) (Float.parseFloat(userWeight.getText().toString()) * 2.204622));
-        editor.putFloat("userHeight", (float) (Float.parseFloat(userHeight.getText().toString()) * 39.3701));
-        userGender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                editor.putInt("userGenderPos", position);
-            }
+        editor.putString("userWeightInKgs", userWeight.getText().toString().trim());
+        editor.putString("userHeightInMeters", userHeight.getText().toString().trim());
+        editor.putFloat("userWeight", (float) (Float.parseFloat(userWeight.getText().toString().trim()) * 2.204622));
+        editor.putFloat("userHeight", (float) (Float.parseFloat(userHeight.getText().toString().trim()) * 39.3701));
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
 
-            }
-        });
-        userActivityLevel.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                editor.putInt("userActivityLevelPos", position);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
         editor.apply();
         if (sharedPreferences.getInt("userGenderPos", 0) == 0) {
             userBMR = 66 + (6.3 * sharedPreferences.getFloat("userWeight", 0) + (12.9 * sharedPreferences.getFloat("userHeight", 0)) - (6.8 * sharedPreferences.getInt("userAge", 0)));
@@ -120,8 +153,30 @@ public class UserInfoActivity extends AppCompatActivity {
         }
         editor.putInt("userBMR", (int) userBMR);
         editor.putInt("caloriesNeed", (int) caloriesNeed);
-//        Snackbar.make(v,"Your BMR is "+ userBMR,Snackbar.LENGTH_SHORT).show();
 
+
+        minCarbohydratesNeed = 0.45 * caloriesNeed / 4.0;
+        maxCarbohydratesNeed = 0.65 * caloriesNeed / 4.0;
+        minProteinsNeed = 0.1 * caloriesNeed / 4.0;
+        maxProteinsNeed = 0.35 * caloriesNeed / 4.0;
+        minFatsNeed = 0.2 * caloriesNeed / 9.0;
+        maxFatsNeed = 0.35 * caloriesNeed / 9.0;
+        editor.putInt("minCarbohydratesNeed", (int) minCarbohydratesNeed);
+        editor.putInt("maxCarbohydratesNeed", (int) maxCarbohydratesNeed);
+        editor.putInt("minProteinsNeed", (int) minProteinsNeed);
+        editor.putInt("maxProteinsNeed", (int) maxProteinsNeed);
+        editor.putInt("minFatsNeed", (int) minFatsNeed);
+        editor.putInt("maxFatsNeed", (int) maxFatsNeed);
+        editor.apply();
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Snackbar.make(v, "Saved Successfully", BaseTransientBottomBar.LENGTH_SHORT);
+                finish();
+            }
+        }, 1100);
 
     }
 
